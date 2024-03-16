5182ac515c3519080e28cc1edbbd2c784da1ea64
Abigail Hartnett <abigailhhartnett@gmail.com>
2024-03-16 11:03:24 -0600

    add .eslintignore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home";
 import PantryPage from "./pages/pantry";
+import ShoppingListPage from "./pages/shoppingList";
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/pantry" element={<PantryPage />} />
+					<Route path="/shopping-list" element={<ShoppingListPage />} />
 				</Routes>
 			</Router>
 		</div>
