e22e4753be6c005bab81995de77af45dc5b16e16
Abigail Hartnett <abigailhhartnett@gmail.com>
2024-03-23 16:52:55 -0600

    fix linting issues

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import IconButton from "./buttons/IconButton";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
