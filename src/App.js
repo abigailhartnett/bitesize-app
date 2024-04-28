20f1bb21022aa1fd57f08e80da42cb3e73d0369a
Abigail Hartnett <abigailhhartnett@gmail.com>
2024-04-27 18:15:49 -0600

    Fix: Fix build errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import supabase from "./config/supabaseClient";
 import HomePage from "./pages/home";
