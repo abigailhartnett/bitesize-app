86d51c2fab3ec07359b85ceef1dc7691d4fdfa11
Abigail Hartnett <abigailhhartnett@gmail.com>
2024-06-15 11:04:32 -0600

    update checkbox

--- src/components/inputs/Checkbox.js
+++ src/components/inputs/Checkbox.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const Checkbox = ({ id, label, onChange, checked }) => {
 	return (
-		<div className="relative">
+		<div className="relative p-4 h-12 w-12 flex items-center justify-center">
 			<input
 				onChange={onChange}
 				checked={checked}
 				type="checkbox"
-				className="hover:cursor-pointer appearance-none h-4 w-4 border-2 border-pepper checked:bg-pepper checked:border-transparent focus:outline-pomegranate peer shrink-0 rounded-sm"
+				className="hover:cursor-pointer appearance-none h-5 w-5 border-2 border-pepper checked:bg-pepper checked:border-transparent focus:outline-pomegranate peer shrink-0 rounded-full"
 				id={id}
 				aria-label={label}
 			></input>
-			<i className="fas fa-check fa-sm text-salt pl-2 hidden peer-checked:block absolute top-2.5 right-0.5 pointer-events-none"></i>
+			<i className="fas fa-check fa-sm text-salt hidden peer-checked:block absolute pointer-events-none top-25% right-25%"></i>
 		</div>
 	);
 };
