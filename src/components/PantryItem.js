10154a4f51f7749098fbf84d5b079509ef46edc4
Abigail Hartnett <abigailhhartnett@gmail.com>
2024-06-15 11:03:02 -0600

    Tidy up PantryItem

--- src/components/PantryItem.js
+++ src/components/PantryItem.js
@@ -14,48 +14,68 @@ const PantryItem = ({
 	addToRecipe,
 	pantryItems,
 	setPantryItems,
+	aisle,
+	status,
+	toggleButton,
+	amount,
+	unit,
 }) => {
-	const shoppingIcon = item.onList ? "fa-circle-check" : "fa-circle-plus";
 	const recipeIcon = "fa-circle-plus";
+	const shoppingIcon = item.onList ? "fa-circle-minus" : "fa-circle-plus";
 
 	return (
 		<div
-			className={`flex py-1 ${item.onList && !showShoppingList ? "text-pepper/50" : ""} ${showShoppingList && item.checked ? "line-through text-pepper/50" : ""}`}
+			className={`grid grid-cols-[auto_1fr_auto] items-center gap-2 py-1 ${showShoppingList && item.checked ? "line-through text-pepper/50" : ""}`}
 		>
-			<div
-				className="flex justify-between hover:font-medium cursor-pointer grow px-4"
-				onClick={() => openPopover(item.id)}
-			>
-				<div className={`flex gap-4 items-center`}>
-					<StatusButton
-						pantryItems={pantryItems}
-						setPantryItems={setPantryItems}
-						item={item}
-						showShoppingList={showShoppingList}
+			<div>
+				{toggleButton && (
+					<IconButton
+						onClick={toggleOnList}
+						icon={shoppingIcon}
+						className={`${item.onList && "text-pepper/50"}`}
 					/>
-					{/* ITEM NAME */}
-					<div className="flex flex-col">
-						<span>{item.name}</span>
-						<span className="text-xs">{item.aisle}</span>
-					</div>
-				</div>
-			</div>
-			{currentPage === "/shopping-list" ? (
-				checkbox && (
+				)}
+				{checkbox && (
 					<Checkbox
 						onChange={onChange}
 						checked={item.checked && true}
 						label="Check off item"
 					/>
-				)
-			) : currentPage === "/create-recipe" ? (
-				<IconButton
-					onClick={addToRecipe}
-					icon={recipeIcon}
-					className={item.onList && !showShoppingList ? "text-pepper/50" : ""}
+				)}
+			</div>
+
+			<div
+				className={`hover:font-medium cursor-pointer`}
+				onClick={() => openPopover(item.id)}
+			>
+				{currentPage === "/create-recipe" ? (
+					<IconButton
+						onClick={addToRecipe}
+						icon={recipeIcon}
+						className={item.onList && "text-pepper/50"}
+					/>
+				) : (
+					""
+				)}
+
+				{/* ITEM NAME */}
+				<div className="flex items-center gap-2">
+					<span>{item.name}</span>
+					{aisle && <span className="text-xs">{item.aisle}</span>}
+					{
+						<span className="text-xs">
+							{amount} {unit}
+						</span>
+					}
+				</div>
+			</div>
+			{status && (
+				<StatusButton
+					pantryItems={pantryItems}
+					setPantryItems={setPantryItems}
+					item={item}
+					showShoppingList={showShoppingList}
 				/>
-			) : (
-				<IconButton onClick={toggleOnList} icon={shoppingIcon} />
 			)}
 		</div>
 	);
