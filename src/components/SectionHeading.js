3bbd41cbf50c40c7617b0915a256d4ad7089e1e6
Abigail Hartnett <abigailhhartnett@gmail.com>
2024-06-24 22:09:19 -0600

    Change spacing on TopBar

--- src/components/SectionHeading.js
+++ src/components/SectionHeading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SectionHeading = ({ children, icon, color }) => {
 	return (
-		<div className="flex items-center border-b border-r-0 border-l-0 border-t-0 border-solid border-pepper/20 pt-4">
+		<div className="flex items-center border-b border-r-0 border-l-0 border-t-0 border-solid border-pepper/20 pt-2">
 			<i class={`${icon} p-4 ${color}`}></i>
 			<h3 class="text-pepper font-semibold text-md capitalize">{children}</h3>
 		</div>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const TopBar = ({ children, pageTitle }) => {
 	return (
-		<div className="pb-4">
-			<h1 className="font-extrabold text-3xl my-6 ml-2 w-full capitalize">
+		<>
+			<h1 className="font-extrabold text-3xl my-4 ml-2 w-full capitalize">
 				{pageTitle}
 			</h1>
 			<div className="flex flex-col gap-2">{children}</div>
-		</div>
+		</>
 	);
 };
 
