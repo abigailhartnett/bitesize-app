a7428d8bf5b9850e2d2b79cf51155aeb122c45fa
Abigail Hartnett <abigailhhartnett@gmail.com>
2024-06-26 18:40:05 -0600

    Fix layout issues

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -3,7 +3,7 @@ import React from "react";
 const ListView = ({ children, grid }) => {
 	return (
 		<div
-			className={`overflow-y-auto ${grid && "grid grid-cols-2 grid-rows-auto gap-4 justify-items-center content-start"}`}
+			className={`overflow-y-auto ${grid ? "grid grid-cols-2 grid-rows-auto gap-4 justify-items-center content-start" : ""}`}
 		>
 			{children}
 		</div>
